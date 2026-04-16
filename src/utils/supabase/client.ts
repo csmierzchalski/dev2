44facba2611942bd9c7547132c913a5c3390838b
csmierzchalski <19324809@brookes.ac.uk>
2026-04-16 12:52:29 +0100

    fix

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,8 @@
     
 import { createBrowserClient } from "@supabase/ssr";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;https://mieqscvyqpdgagqjwazi.supabase.co
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;sb_publishable_Fh2JkitEAHC0IV3t9tGPYQ_yKM80uEB
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL = https://mieqscvyqpdgagqjwazi.supabase.co ; 
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY = sb_publishable_Fh2JkitEAHC0IV3t9tGPYQ_yKM80uEB;
 
 export const supabaseBrowserClient = createBrowserClient(
   supabaseUrl!,
